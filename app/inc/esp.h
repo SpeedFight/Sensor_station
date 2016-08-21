3c46e82a7243722255de631eb902cbf62ebd828e
klukasiewicz <speedfight_2@wp.pl>
2016-08-21 15:03:24 +0200

    dodano funkcje do logowania wifi,tcp. dodano kilka protypow funkcji, struktora do esp, pomniejsze zmiany

--- app/inc/esp.h
+++ app/inc/esp.h
@@ -3,3 +3,13 @@
  * @Author	SpeedFight (speedfight_2@wp.pl)
  * @date	19.08.16
  */
+
+typedef struct{
+        void (*connect_to_wifi)(char *ssid, char *password);
+        void (*test_connection)();
+        void (*connect_to_TCP)(char *ip, char *port);
+        //void (*send_data)();
+
+}esp_typedef;
+
+void esp_init(void (*uart_send_function)(char *));
--- app/src/esp.c
+++ app/src/esp.c
@@ -4,29 +4,78 @@
  * @date	19.08.16
  */
 #include "../inc/esp.h"
+#include <avr/io.h>
+#include <util/delay.h>
 
 #include "stdio.h"
+
+
+#define TMP_BUFF_SIZE 128
+#define DELAY_TIME 750
+
+static void (*send_uart)(char *);    //pointer to send via uart function
+static char tmp_buff[TMP_BUFF_SIZE];    //buffer for all cstrings
+
 
 /**
  * @brief
  * @detail
  */
-static void send_esp(      void (*send_function)(char *),  /*function pointer*/
-                char *message)                  /*message (cstring)*/
+void esp_init(void (*uart_send_function)(char *))  //function pointer
 {
-        send_function(message);
+        send_uart=uart_send_function;
 }
 
-//AT command list
+static void log_to_wifi(char *ssid,char *password)
+{
+        send_uart("AT+CWMODE=1\n\r");
+        snprintf(tmp_buff, TMP_BUFF_SIZE,"AT+CWJAP=\"%s\",\"%s\"\n\r",ssid,password);   //format text to log into wifi
+        _delay_ms(DELAY_TIME);
+        send_uart(tmp_buff);
+
+        //sprawdzenie czy sie udało++
+}
+
+static uint8_t ping()
+{
+        send_uart("AT+PING=\"www.google.pl\"\n\r");
+
+        //sprawdzanie czy pozytywne++
+        return 0;
+}
+
+static void log_to_TCP(char *ip, char *port)
+{
+        send_uart("AT+CIPMODE=0\n\r");
+        _delay_ms(DELAY_TIME);
+        send_uart("AT+CIPMUX=0\n\r");
+        _delay_ms(DELAY_TIME);
+        snprintf(tmp_buff, TMP_BUFF_SIZE,"AT+CIPSTART=\"TCP\",\"%s\",%s\n\r",ip,port);   //format text to log into TCP
+        _delay_ms(DELAY_TIME);
+
+        //sprawdzenie czy sie udało++
+}
+
+static void send()
+{
+        //log to TCP++
+        //format data++
+        //send_data++
+        //logout from TCP++
+}
+
+
+
+//AT command list"
 /*always end by \n\r */
-char *cwmode1="AT+CWMODE=1\n\r";
-char *cwjap="AT+CWJAP=\"SSID\",\"pass\"\n\r";
-char *ping="AT+PING=\"www.google.pl\"\n\r";
+//char *cwmode1="AT+CWMODE=1\n\r";
+//char *cwjap="AT+CWJAP=\"SSID\",\"pass\"\n\r";
+//char *ping="AT+PING=\"www.google.pl\"\n\r";
 
-char *cipmode0="AT+CIPMODE=0\n\r";
-char *cipmux0="AT+CIPMUX=0\n\r";
+//char *cipmode0="AT+CIPMODE=0\n\r";
+//char *cipmux0="AT+CIPMUX=0\n\r";
 
-char *cipstart="AT+CIPSTART=\"TCP\",\"184.106.153.149\",80\n\r";
+//char *cipstart="AT+CIPSTART=\"TCP\",\"184.106.153.149\",80\n\r";
 
 char *cipsend="AT+CIPSEND=73\n\r";
 
