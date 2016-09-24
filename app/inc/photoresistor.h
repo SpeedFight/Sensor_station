e683bf185d9e5dc49a77e75479877f145a576fbc
speedfight <speedfight_2@wp.pl>
2016-09-24 19:42:05 +0200

    dodanie plikow dla fotorezystora

---
+++ app/inc/photoresistor.h
@@ -0,0 +1,12 @@
+/**
+* @file	photoresistor.h
+* @Author	SpeedFight (speedfight_2@wp.pl)
+* @date	24.09.16
+*/
+
+#ifndef _photoresistor_h
+#define _photoresistor_h
+
+#include <avr/io.h>
+
+#endif
---
+++ app/src/photoresistor.c
@@ -0,0 +1,8 @@
+/**
+* @file	photoresistor.c
+* @Author	SpeedFight (speedfight_2@wp.pl)
+* @date	24.09.16
+*/
+
+#include "../inc/photoresistor.h"
+#include <avr/io.h>
