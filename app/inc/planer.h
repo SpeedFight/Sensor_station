d373e5ba05b05ea926d74205be3a3d7c7b71997c
speedfight <speedfight_2@wp.pl>
2016-09-25 13:49:53 +0200

    pliki planera dodane

---
+++ app/inc/planer.h
@@ -0,0 +1,13 @@
+/**
+* @file	planer.h
+* @Author	SpeedFight (speedfight_2@wp.pl)
+* @date	25.09.16
+*/
+
+#ifndef _planer_h
+#define _planer_h
+
+#include <avr/io.h>
+
+
+#endif
---
+++ app/src/planer.c
@@ -0,0 +1,11 @@
+/**
+* @file	planer.c
+* @Author	SpeedFight (speedfight_2@wp.pl)
+* @date	25.09.16
+*/
+
+#include <stdlib.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "../inc/planer.h"
